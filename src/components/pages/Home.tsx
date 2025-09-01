e2eac0860caa3e4ba2d29cde678d7bfdc0d3237a
Tom Schindler <tom.schindler@student.uni-halle.de>
2025-09-01 16:12:32 +0200

    Image responsive Home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <section className='text-justify'>
-        <img src={action10Img} className='w-150 h-150' alt="Aktion 10%" />
+        <img src={action10Img} className='w-150' alt="Aktion 10%" />
       </section>
     </>
   )
