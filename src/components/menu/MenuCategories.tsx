ad8c8e925c22c87d5d6443c2023fa280ac5ac927
Tom Schindler <tom.schindler@student.uni-halle.de>
2025-09-05 16:43:47 +0200

    Menü Categorie Fehler behoben

--- src/components/menu/MenuCategories.tsx
+++ src/components/menu/MenuCategories.tsx
@@ -24,22 +24,24 @@ const MenuCategories = () => {
                 <motion.div key={category} onHoverStart={() => setActiveCategory(category)} onHoverEnd={() => setActiveCategory(null)} className='relative' >
 
                     {subCategories.length >= 2 && (
-                        <div className={activeCategory === category ? 'bg-neutral-200 dark:bg-neutral-700 rounded-t-lg' : ''}>
-                            <Button variant='ghost' className=' hover:bg-neutral-200/90 dark:hover:bg-neutral-700/90 cursor-pointer font-bold text-lg' 
-                                onClick={() => handleCategoryClick(category)}>{category}
-                            </Button>
-                        </div>
-                    )}
+                        <>
+                            <div className={activeCategory === category ? 'bg-neutral-200 dark:bg-neutral-700 rounded-t-lg ring-2' : ''}>
+                                <Button variant='ghost' className=' hover:bg-neutral-200/90 dark:hover:bg-neutral-700/90 cursor-pointer font-bold text-lg' 
+                                    onClick={() => handleCategoryClick(category)}>{category}
+                                </Button>
+                            </div>
 
-                    {activeCategory === category && (
-                        <div className="absolute top-9 p-2 bg-neutral-200 dark:bg-neutral-700 rounded-b-lg rounded-tr-lg shadow-lg z-10">
-                            {subCategories.map((subCategory) => (
-                                <CategoryButton onClick={() => setActiveCategory(null)}
-                                    key={subCategory.category}
-                                    title={subCategory.category}
-                                />
-                            ))}
-                        </div>
+                            {activeCategory === category && (
+                                <div className="absolute top-9 p-2 bg-neutral-200 dark:bg-neutral-700 rounded-b-lg rounded-tr-lg shadow-lg z-10 ring-2">
+                                    {subCategories.map((subCategory) => (
+                                        <CategoryButton onClick={() => setActiveCategory(null)}
+                                            key={subCategory.category}
+                                            title={subCategory.category}
+                                        />
+                                    ))}
+                                </div>
+                            )}
+                        </>
                     )}
 
                     {subCategories.length === 1 &&
