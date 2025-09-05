baeb0221947479a0b9518c8ed424d715dff10f6f
Tom Schindler <tom.schindler@student.uni-halle.de>
2025-09-05 16:43:23 +0200

    neuer Aktionsbanner

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,4 @@
-import action10Img from '../../assets/action10.png'
-import action20Img from '../../assets/action20.png'
+import action10Img from '../../assets/action10Banner.png'
 import SaleTab from '../home/SaleTab';
 import RecommendationTab from '../home/RecommendationTab';
 
@@ -8,8 +7,7 @@ const Home = () => {
 	return (
 		<section className='flex flex-col gap-5 md:gap-8'>
 			<div className='flex gap-1 md:gap-10 lg:gap-16 justify-around'>
-				<img src={action10Img} className='w-140 min-w-10' alt="Aktion 10%" />
-				<img src={action20Img} className='w-140 min-w-10' alt="Aktion 20%" />
+				<img src={action10Img} className=' min-w-10' alt="Aktion 10%" />
 			</div>
 			
 			<SaleTab />
