5a7ab021a62de15545cda769e500db386009f120
Tom Schindler <tom.schindler@student.uni-halle.de>
2025-09-06 11:34:09 +0200

    About und Contact Seite erstellt

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,7 +1,29 @@
 const About = () => {
   	
 	return (
-    	<div>About</div>
+		<section>
+			<h1 className="text-4xl font-bold mb-8">About</h1>
+			<div className="space-y-7 min-w-40 max-w-150 text-xl text-justify">
+				<div className="space-y-3">
+					<h3 className="text-2xl font-bold">About Memolya</h3>
+					<p>
+						Memolya ist ein fiktiver Online-Shop, der eine vielfältige Auswahl an Produkten an einem Ort vereint. Von moderner Elektronik über aktuelle Mode bis hin zu Fahrzeugzubehör und Beauty-Artikeln - bei Memolya soll für jede und jeden etwas dabei sein.
+					</p>
+				</div>
+				<div className="space-y-3">
+					<h3 className="text-2xl font-bold">Our Goals</h3>
+					<p>
+						Unser Ziel ist es, das Einkaufen so einfach wie möglich zu gestalten: klare Kategorien, kurze Wege zum Produkt und ein übersichtliches Design. Auch wenn Memolya nur als Demo-Projekt existiert, steht der Shop für die Idee, Vielfalt und Komfort miteinander zu verbinden.
+					</p>
+				</div>
+				<div className="space-y-3">
+					<h3 className="text-2xl font-bold">Discover Diversity</h3>
+					<p>
+						Bei Memolya legen wir Wert auf Übersichtlichkeit, Einfachheit und Nutzerfreundlichkeit. Jede Kategorie ist klar strukturiert, damit Besucher schnell finden, was sie suchen. Unser fiktiver Shop zeigt, wie ein modernes Einkaufserlebnis aussehen kann - effizient, angenehm und ohne Ablenkungen.
+					</p>
+				</div>
+			</div>
+		</section>
   	)
 }
 
--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,7 +1,19 @@
 const Contact = () => {
 	
 	return (
-		<div>Contact</div>
+		<section className="space-y-5">
+			<h1 className="text-4xl font-bold mb-6">Contact</h1>
+
+			<p className="min-w-40 max-w-150 text-xl text-justify">Memolya is a fictitious online shop. We are symbolically available for questions, suggestions, or feedback. The contact details below are fictitious and are for demonstration purposes only.</p>
+
+			<h3 className="text-2xl font-bold mb-2">Impressum</h3>
+			<div className="space-y-2 mt-5 text-lg">
+				<p><span className="font-bold">Adresse:</span> Musterstraße 12, 12345 Musterstadt</p>
+				<p><span className="font-bold">Telefon:</span> +49 123 45678908312</p>
+				<p><span className="font-bold">E-Mail:</span> info@muster-memolya.de</p>
+				<p><span className="font-bold">Öffnungszeiten:</span> Mo-Fr 09:00-18:00 Uhr</p>
+			</div>
+		</section>
 	)
 }
 
